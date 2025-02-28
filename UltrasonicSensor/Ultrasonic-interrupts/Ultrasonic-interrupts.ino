ad690075cf342d832acae5037ad5a04c13ae4a20
geojax <micha.rand@gmail.com>
2025-02-28 14:20:58 -0800

    back to blocking code in bursts

--- UltrasonicSensor/Ultrasonic-interrupts/Ultrasonic-interrupts.ino
+++ UltrasonicSensor/Ultrasonic-interrupts/Ultrasonic-interrupts.ino
@@ -5,39 +5,42 @@
 #include "TimerInterrupt.h"
 #include "ISR_Timer.h"
 
-float distance, duration;
+float distance;
 long avgLast4 = 0;
-long last2Avgs[2] = {0,0};
-long last4[4] = {0,0,0,0};
+// long last2Avgs[2] = {0,0};
+// long last4[4] = {0,0,0,0};
+uint32_t readings[32];
 uint32_t n = 0; 
 uint32_t m = 0;
 long pulseBeginTimeMicros = 0;
-void ultrasonicCallback() {
-  long duration = micros() - pulseBeginTimeMicros;
-  // Serial.print("Sensor read ");
-  // Serial.println(duration);
-  avgLast4 += duration >> 2;
-  if (n == 3) {
-    avgLast4 >>= 1; // >> 2 to average, then another >> 1 for consistency.
-    // Serial.print(avgLast4);
-    // Serial.print(",");
-    // Serial.print(last2Avgs[1]);
-    // Serial.print(",");
-    // Serial.println(last2Avgs[0]);
+// void ultrasonicCallback() {
+//   long duration = micros() - pulseBeginTimeMicros;
+//   avgLast4 += duration >> 2;
+//   if (n == 3) {
+//     avgLast4 >>= 1; // >> 2 to average, then another >> 1 for consistency.
 
-    if ((avgLast4 > (last2Avgs[1] + 1)) && (last2Avgs[0] > last2Avgs[1] + 1))
-      {Serial.print(m++); Serial.println("min found!");
-      Serial.print(avgLast4);
-      Serial.print(",");
-      Serial.print(last2Avgs[1]);
-      Serial.print(",");
-      Serial.println(last2Avgs[0]);}
+//     if ((avgLast4 > (last2Avgs[1] + 1)) && (last2Avgs[0] > last2Avgs[1] + 1))
+//       {Serial.print(m++); Serial.println("min found!");
+//       Serial.print(avgLast4);
+//       Serial.print(",");
+//       Serial.print(last2Avgs[1]);
+//       Serial.print(",");
+//       Serial.println(last2Avgs[0]);}
 
-    last2Avgs[0] = last2Avgs[1];
-    last2Avgs[1] = avgLast4;
-  }
-  n = (n < 3) ? n + 1 : 0;
+//     last2Avgs[0] = last2Avgs[1];
+//     last2Avgs[1] = avgLast4;
+//   }
+//   n = (n < 3) ? n + 1 : 0;
   
+// }
+
+long avgRead() {
+  long avg = 0;
+  for (int i = 0; i < 4; ++i) {
+    ultraStartCallback(); // Send out an ultrasonic wave.
+    avg += pulseIn(PIN_ECHO, HIGH) >> 3;
+  }
+  return avg;
 }
 
 void ultraStartCallback() {
@@ -52,7 +55,7 @@ void ultraStartCallback() {
 
 void setup() {
   ITimer1.init();
-  ITimer1.attachInterrupt(100.0, ultraStartCallback); // 10 times per second
+  // ITimer1.attachInterrupt(100.0, ultraStartCallback); // 10 times per second
   // put your setup code here, to run once:
   pinMode(PIN_TRIG, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
@@ -61,9 +64,9 @@ void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  attachInterrupt(digitalPinToInterrupt(PIN_ECHO),
-    ultrasonicCallback,
-    FALLING);
+  // attachInterrupt(digitalPinToInterrupt(PIN_ECHO),
+  //   ultrasonicCallback,
+  //   FALLING);
 
   // Generate first ultrasonic wave.
   digitalWrite(PIN_TRIG, LOW);
@@ -77,4 +80,9 @@ void setup() {
 void loop() {
   // ensure ultrasonic sensor has had a read before next emission.
   // delay(100);
+  if (Serial.read() != -1) {
+    int read = avgRead();
+    Serial.println(read);
+    readings[n++] = read;
+  }
 }
